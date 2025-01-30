a74d69c65035519dc240ee7852bca7975365936f
Onesmartshark <onesmartshark@gmail.com>
2025-01-29 21:04:07 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,10 +1,4 @@
 addLayer("s", {
-    upgrades: {
-        11: {
-            description: "More Grass 1.5x Grass",
-            name: "b",
-        },
-    },
     name: "soil", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -30,5 +24,11 @@ addLayer("s", {
     hotkeys: [
         {key: "s", description: "S: Reset for soil", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    upgrades: {
+        11: {
+            description: "More Grass 1.5x Grass",
+            name: "b",
+        },
+    },
     layerShown(){return true}
 })
